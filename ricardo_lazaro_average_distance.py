d39c07abeecaf37f838ddbc42247b103efd9bff7
ricklaz <33107229+ricklaz@users.noreply.github.com>
2020-09-05 17:01:36 -0300

    Update ricardo_lazaro_average_distance.py

--- ricardo_lazaro_average_distance.py
+++ ricardo_lazaro_average_distance.py
@@ -1,5 +1,5 @@
-#Código que faz parte dos teste para a Fucional
-# Ricardo Lázaro - 03/09/2020
+#Código que faz parte dos teste para a Fucional - escrito por Ricardo Lázaro - 03/09/2020
+#Calcula a média de distancia percorrido em viagens feitas por até 2 passageiros
 
 #-*- coding: utf-8 -*-
 
@@ -10,22 +10,20 @@ from datetime import date
 # crio variáveis para conter os nomes dos arquivos físicos
 csvfull = "2018_Yellow_Taxi_Trip_Data.csv"
 
-# crio um df lendo os dados do arquivo csv filtrando apenas as colunas que preciso 
-#df = pd.read_csv(csvfull, usecols=['VendorID', 'trip_distance', 'passenger_count' ]) 
+# crio um df lendo os dados do arquivo csv
 df = pd.read_csv(csvfull, usecols=[0, 1, 2, 3, 4]) 
 
+#converto as colunas de data para o formato padrão
+df[['tpep_pickup_datetime','tpep_dropoff_datetime']] = df[['tpep_pickup_datetime','tpep_dropoff_datetime']].apply(pd.to_datetime,format='%m/%d/%Y %H:%M:%S %p')
+
 # filtro o número de passagheiros para ATÉ 2, conforme solicitado no teste
-df2 = df[(df['passenger_count'] <= 2)]
+df = df[(df['passenger_count'] <= 2)]
 
 # obtenho o total de viagens deste data frame filtrado
-viagens = df2['VendorID'].count() #ou rint(len(df2))
+viagens = df['VendorID'].count() #ou print(len(df2))
 
 # calculo a média da distância percorrida pelos táxis
-media = round(df2['trip_distance'].mean(),2)
+distancia_media = round(df2['trip_distance'].mean(),2)
 
 # exibição dos resultados finais
-print("RESULTADOS  DA PESQUISA: Foram feitas "+ str(viagens) + " viagens com até 2 passageiros e obtivemos uma média de " + str(media) + "km percorridos!")
-
-print(date.weekday(date.today()))
-#print(date.today())
-#print(date.weekday(df["tpep_pickup_datetime"]))
+print("RESULTADOS  DA PESQUISA: Foram feitas "+ str(viagens) + " viagens com até 2 passageiros e obtivemos uma média de " + str(distancia_media) + "km percorridos!")
